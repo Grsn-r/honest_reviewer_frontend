4ed20a4643d442bf4eae05beb3ef097d5170bf35
grsn_ <gers.e02@gmail.com>
2026-04-25 18:21:05 -0600

    nombres de componentes corregidos

--- src/components/main/reviews/Main.jsx
+++ src/components/main/reviews/Main.jsx
@@ -1,13 +1,13 @@
 
-import Review from './Review.jsx'
+import Review from './Review';
 import {Children, useContext} from 'react';
-import UserContext from '../../../context/userContext.js';
+import UserContext from '../../../context/userContext';
 import edit from '../../../images/edit.svg';
 import setPw from '../../../images/setPw.svg';
-import Popup from '../../popups/Popup.jsx';
-import EditUserInfo from '../../popups/editUserInfo.jsx';
-import SetPasswordPopup from '../../popups/SetPasswordPopup.jsx';
-import PostReviewPopup from '../../popups/PostReviewPopup.jsx';
+import Popup from '../../popups/Popup';
+import EditUserInfo from '../../popups/EditUserInfo';
+import SetPasswordPopup from '../../popups/SetPasswordPopup';
+import PostReviewPopup from '../../popups/PostReviewPopup';
 
 function Main({onOpenPopup}) {
     const {user, popup, handleClosePopup, reviews} = useContext(UserContext);
