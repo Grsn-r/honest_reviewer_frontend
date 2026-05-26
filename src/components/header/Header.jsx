2196d0f67fd8d4a7a670b872fa26d5164e4df213
grsn_ <gers.e02@gmail.com>
2026-05-26 13:02:19 -0600

    se añade logo al header, box-shado implementado para mejora visual de interfaz

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,13 @@
+import logo from '../../images/pageLogo.svg'
 
 function Header({logged, logout}) {
     return (
         <header className="header">
+             <img src={logo} alt='logo' />
             <div className="header__text">
                 <p className='header__info_element'>Haz una reseña HONESTA</p>
             </div>
+           
             <div className="header__sesion">
                 {logged && (<button className="header__sesion_logout" onClick={logout}>Cerrar Sesión</button>)}
             </div>
